bcfda8aef96a84e00e0d79d87870c319b051e114
Wallace Coelho <wac.almeida@gmail.com>
2016-12-29 15:00:02 -0200

    Primeiro Commit

---
+++ app.js
@@ -0,0 +1,64 @@
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(initialize);
+    } else {
+        alert("Não é possível pegar sua posição");
+    }
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var map;
+var service;
+var infowindow;
+
+function initialize(position) {
+  var pos = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+
+  map = new google.maps.Map(document.getElementById('map'), {
+      center: pos,
+      zoom: 15
+    });
+
+  var request = {
+    location: pos,
+    radius: '500',
+    types: ['store'],
+	type: 'restaurant'
+  };
+
+  service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, callback);
+}
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    /*for (var i = 0; i < results.length; i++) {
+      var place = results[i];
+      console.log('point'+i, results[i]);
+	  createMarker(map, results[i]);
+    }*/
+	var num = getRandomInt(0, results.length-1);
+	createMarker(map, results[num]);
+  }
+}
+
+function createMarker (map, object) {
+	var marker = new google.maps.Marker({
+	  position: object.geometry.location,
+	  map: map,
+	  title: object.name
+	});
+	contentString = 
+		"Nome: "+object.name+"\n" +
+		"Endereço: "+object.vicinity+"\n";
+		
+	var infowindow = new google.maps.InfoWindow({
+		content: contentString
+	  });
+	marker.addListener('click', function() {
+		infowindow.open(map, marker);
+	  });
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+#Restaurante do Dia
+
+Não sabe onde almoçar hoje? Este projeto pesquisa restaurantes nos arredores e te trás um aleatório :)
